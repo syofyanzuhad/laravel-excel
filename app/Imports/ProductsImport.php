8d87fbada44bd0483707b1e8b1c6ccc768d3bbb2
syofyanzuhad <sofyanzuhad2@gmail.com>
2020-12-04 04:53:09 +0700

    Fixed: added import excel

---
+++ app/Imports/ProductsImport.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Imports;
+
+use App\Product;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\Importable;
+
+class ProductsImport implements ToModel
+{
+    use Importable;
+
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Product([
+            'title' => $row[0],
+            'slug' => str_slug($row[0]),
+            'description' => $row[1],
+            'price' => $row[2],
+            'stock' => $row[3]
+        ]);
+    }
+}
--- database/migrations/2020_12_03_145420_create_products_table.php
+++ database/migrations/2020_12_03_145420_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->integer('price');
+            $table->integer('stock');
             $table->timestamps();
         });
     }
